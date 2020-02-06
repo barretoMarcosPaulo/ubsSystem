34d5bc976f340b6c79c909039154aebcd6cbe823
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-05 23:05:30 -0200

    views para vinvular especialidas a um medico

--- ubs/specialty/models.py
+++ ubs/specialty/models.py
@@ -6,13 +6,16 @@ from ubs.accounts.models import Doctor
 class MedicalSpecialty(AuditModel):
     desc_specialty = models.CharField('Descrição especialidade(s)',max_length=100)
 
+    def __str__(self):
+        return self.desc_specialty
+
     class Meta:
         verbose_name = 'Especialidade Médica'
         verbose_name_plural = 'Especialidades Médicas'
 
 class DoctorHasMedicalSpecialty(AuditModel):
-    doctor = models.ManyToManyField(Doctor,verbose_name='Médico')
-    MedicalSpecialty_idSpecialty = models.ForeignKey(MedicalSpecialty,verbose_name='Especialidade',null=True,blank=True,on_delete=models.SET_NULL)
+    doctor = models.ForeignKey(Doctor,verbose_name='Médico',null=True,blank=True,on_delete=models.SET_NULL)
+    MedicalSpecialty_idSpecialty = models.ManyToManyField(MedicalSpecialty,verbose_name='Especialidade')
 
     class Meta:
         verbose_name = 'Especialidade'
--- ubs/specialty/views.py
+++ ubs/specialty/views.py
@@ -79,4 +79,68 @@ class  SpecialtyDelete(DeleteView):
 
 
 
+class DoctorSpecialtyCreate(CreateView):
+    model = DoctorHasMedicalSpecialty
+    template_name = 'doctor_specialty/add.html'
+    form_class = DoctorSpecialtyForm
+
+    def get_success_url(self):
+        return reverse('specialty:list_doctor_specialty')
+
+class DoctorSpecialtyList(ListView):
+
+    model = DoctorHasMedicalSpecialty
+    http_method_names = ['get']
+    template_name = 'doctor_specialty/list.html'
+    context_object_name = 'object_list'
+    paginate_by = 20
+
+    def get_queryset(self):
+        self.queryset = super(DoctorSpecialtyList, self).get_queryset()
+        if self.request.GET.get('search_box', False):
+            self.queryset=self.queryset.filter(Q(full_name__icontains = self.request.GET['search_box']) | Q(first_name__icontains=self.q))
+        return self.queryset
+
+    def get_context_data(self, **kwargs):
+        _super = super(DoctorSpecialtyList, self)
+        context = _super.get_context_data(**kwargs)
+        adjacent_pages = 3
+        page_number = context['page_obj'].number
+        num_pages = context['paginator'].num_pages
+        startPage = max(page_number - adjacent_pages, 1)
+        if startPage <= 5:
+            startPage = 1
+        endPage = page_number + adjacent_pages + 1
+        if endPage >= num_pages - 1:
+            endPage = num_pages + 1
+        page_numbers = [n for n in range(startPage, endPage) \
+            if n > 0 and n <= num_pages]
+        context.update({
+            'page_numbers': page_numbers,
+            'show_first': 1 not in page_numbers,
+            'show_last': num_pages not in page_numbers,
+            })
+        return context
+
+
+class DoctorSpecialtyUpdate(UpdateView):
+    model = DoctorHasMedicalSpecialty
+    template_name = 'doctor_specialty/edit.html'
+    form_class = DoctorSpecialtyForm
+
+    def get_success_url(self):
+        return reverse('specialty:list_doctor_specialty')
+
+
+class  DoctorSpecialtyDelete(DeleteView):
+    model = DoctorHasMedicalSpecialty
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+            self.object.delete()
+            return JsonResponse({'msg': "especialidade excluida com sucesso!", 'code': "1"})
+        except:
+            return JsonResponse({'msg': "Essa especialidade não pôde ser excluída!", 'code': "0"})
+
 
