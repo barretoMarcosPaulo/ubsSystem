56123bf4b441d9a949346c00ca4de1801403e262
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-02-21 12:28:46 -0300

    feat: importaçãod de dados via arquivo para tabela CID10

--- ubs/medical_query/admin.py
+++ ubs/medical_query/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Query
+from .models import Query, CID10
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources,fields
+from import_export.widgets import ForeignKeyWidget
 
-admin.site.register(Query)
+admin.site.register(Query) 
+
+class CID10Resource(resources.ModelResource):
+    class Meta:
+        import_id_fields = ('idCID10',)
+        model = CID10
+        exclude = ('created_on', 'updated_on')
+
+@admin.register(CID10)
+class CID10Resource(ImportExportModelAdmin):
+    resource_class = CID10Resource
