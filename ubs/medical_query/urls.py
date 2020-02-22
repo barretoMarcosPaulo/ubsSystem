f79b9ba9297402c69ffb1b4527ac690f48e1e16f
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-21 23:46:47 -0300

    passando pk na url

--- ubs/medical_query/urls.py
+++ ubs/medical_query/urls.py
@@ -4,12 +4,14 @@ from . import views
 app_name = 'medical_query'
 
 urlpatterns = [
-    path('consulta/add', views.QueryCreate.as_view(), name='add_query'),
+    path('consultar/<int:pk>/', views.QueryCreate.as_view(), name='add_query'),
     path('consulta-historico/list', views.ListQuerysHistory.as_view(), name='list_query_history'),
     path('consulta/edit/<int:pk> ', views.QueryUpdate.as_view(), name='update_query'),
     path('consulta/detalhes/<int:pk> ', views.Attendances.as_view(), name='detail_query'),
     path('atendimentos/list', views.ListAttendances.as_view(), name='list_attendances'),    
 
     path('encaminhamento/', views.ForwardingCreate.as_view(), name='forwarding_create'),    
-    path('encaminhamentos-realizados/', views.ForwardingList.as_view(), name='currents_forwarding'),    
+    path('encaminhamentos-realizados/', views.ForwardingList.as_view(), name='currents_forwarding'),
+
+    path('atendimentos-dia/', views.AwaitQuerys.as_view(), name='await_querys'),    
 ]
