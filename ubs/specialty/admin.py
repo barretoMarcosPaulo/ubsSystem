c238a32dbb11f2e2293228953e3be7d5f48e8ade
lucasbezerra26 <lucasbezerra527@gmail.com>
2020-02-23 19:02:29 -0300

    feat: Importação de todos os arquivos que está na lista FilesToImport no trello

--- ubs/specialty/admin.py
+++ ubs/specialty/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources,fields
+
+class MedicalSpecialtyResource(resources.ModelResource):
+    class Meta:
+        model = MedicalSpecialty
+        import_id_fields = ('desc_specialty',)
+        exclude = ('id','created_on', 'updated_on')
+
+@admin.register(MedicalSpecialty)
+class MedicalSpecialtyResource(ImportExportModelAdmin):
+    resource_class = MedicalSpecialtyResource
