7a4527867d9f4942b523e416a5275fe9eea56b27
Marcos <marcospsbarreto@ufpi.edu.br>
2022-06-26 20:10:16 -0300

    feat: black formatter

--- ubs/core/models.py
+++ ubs/core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class AuditModel(models.Model):
-	created_on = models.DateField('Criado em', auto_now_add=True)
-	updated_on = models.DateField('Autalizado em', auto_now=True)
 
-	class Meta:
-		abstract=True
+class AuditModel(models.Model):
+    created_on = models.DateField("Criado em", auto_now_add=True)
+    updated_on = models.DateField("Autalizado em", auto_now=True)
+
+    class Meta:
+        abstract = True
