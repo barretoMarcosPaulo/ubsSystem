cc3d4055be14bcbdf3757f95980152c097408ac8
Marcos <marcospsbarreto@ufpi.edu.br>
2022-06-26 20:09:54 -0300

    feat: change crm for not required field

--- ubs/accounts/models.py
+++ ubs/accounts/models.py
@@ -1,9 +1,10 @@
 import re
 import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.core import validators
 from django.db import models
 from django.urls import reverse
-from django.core import validators
-from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 from ubs.core.models import AuditModel
 from ubs.specialty.models import DoctorHasMedicalSpecialty
 
@@ -11,38 +12,43 @@ from ubs.specialty.models import DoctorHasMedicalSpecialty
 class User(AbstractBaseUser, PermissionsMixin, AuditModel):
 
     username = models.CharField(
-        'Usuário', max_length=200, default=uuid.uuid4, unique=True, validators=[
-        validators.RegexValidator(
-            re.compile('^[\w.@+-]+$'),
-            'Informe um nome de usuário válido. '
-            'Este valor deve conter apenas letras, números '
-            'e os caracteres: @/./+/-/_ .'
-            , 'invalid'
-        )
-        ], help_text='Um nome curto que será usado para identificá-lo de forma única na plataforma'
+        "Usuário",
+        max_length=200,
+        default=uuid.uuid4,
+        unique=True,
+        validators=[
+            validators.RegexValidator(
+                re.compile("^[\w.@+-]+$"),
+                "Informe um nome de usuário válido. "
+                "Este valor deve conter apenas letras, números "
+                "e os caracteres: @/./+/-/_ .",
+                "invalid",
+            )
+        ],
+        help_text="Um nome curto que será usado para identificá-lo de forma única na plataforma",
     )
-    full_name = models.CharField('Nome Completo',max_length=100)
-    cpf = models.CharField('CPF', max_length=14, unique="True")
-    phone = models.CharField('Telefone', max_length=16)
-    email = models.EmailField('Email',max_length=100,unique="True")
-    is_staff = models.BooleanField('is staff',default=False)
-    is_clerk = models.BooleanField('Atendente',default=False)
-    is_doctor = models.BooleanField('Medico',default=False)
+    full_name = models.CharField("Nome Completo", max_length=100)
+    cpf = models.CharField("CPF", max_length=14, unique="True")
+    phone = models.CharField("Telefone", max_length=16)
+    email = models.EmailField("Email", max_length=100, unique="True")
+    is_staff = models.BooleanField("is staff", default=False)
+    is_clerk = models.BooleanField("Atendente", default=False)
+    is_doctor = models.BooleanField("Medico", default=False)
 
     def __str__(self):
         return self.full_name
-    
+
     objects = UserManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
 
     def get_absolute_url(self):
-        return reverse('accounts:list_all_admin')
+        return reverse("accounts:list_all_admin")
 
     def get_short_name(self):
-        return self.full_name.split(' ')[0]
-        
+        return self.full_name.split(" ")[0]
+
     def is_medical(self):
         try:
             Doctor.objects.get(id=self.id)
@@ -50,21 +56,20 @@ class User(AbstractBaseUser, PermissionsMixin, AuditModel):
         except:
             return False
 
-
     class Meta:
-        verbose_name = 'Usuário'
-        verbose_name_plural = 'Usuários'
-    
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+
 
 class Clerk(User):
-    register_clerk = models.CharField('registro do técnico', max_length=12)
+    register_clerk = models.CharField("registro do técnico", max_length=12)
 
     def get_absolute_url(self):
-        return reverse('accounts:list_all_clerk')  
+        return reverse("accounts:list_all_clerk")
 
 
 class Doctor(User):
-    crm_doc = models.CharField('CRM do médico', max_length=20)
+    crm_doc = models.CharField("CRM do médico", max_length=20, blank=True, null=True)
 
     def get_specialts(self):
         list_specialts = []
@@ -78,5 +83,4 @@ class Doctor(User):
         return list_specialts
 
     def get_absolute_url(self):
-        return reverse('accounts:list_all_doctor')  
-
+        return reverse("accounts:list_all_doctor")
